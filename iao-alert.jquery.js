4e31ffeb8297dcbd7eaa86bf789406c2fbdc3b02
Prashant Kapoor <prashant@hatwada.com>
2017-01-23 03:00:15 +0530

    Adding on cross click close functionality js.

--- iao-alert.jquery.js
+++ iao-alert.jquery.js
@@ -1,7 +1,7 @@
 /*
  Plugin Name: iao alert
          Key: iao-alert
-     Version: 1.0.0
+     Version: 1.0.1
       Author: Prashant Kapoor
      Website: http://www.itsallonly.com
         Repo: https://github.com/Itsallonly/iao-alert
@@ -18,7 +18,20 @@
         }, arr );
     	if($('iao-alert-box').length==0)
     	$('body').append('<iao-alert-box><iao-alert-start></iao-alert-start></iao-alert-box>');
-    	$('<iao-alert mode="'+opt.mode+'"type="'+opt.type+'">'+opt.msg+'</iao-alert>').insertAfter('iao-alert-start').delay(3000).fadeOut(500, function(){$(this).remove();});
+    	var iaoAlert = $('<iao-alert mode="'+opt.mode+'"type="'+opt.type+'">'+opt.msg+'<iao-alert-close></iao-alert-close></iao-alert>')
+    	.insertAfter('iao-alert-start');
+    	setTimeout(function(){
+    		iaoAlert.fadeOut(500, function() {
+	    		$(this).remove();
+	    	});
+    	}, 3000);
+    	$('iao-alert > iao-alert-close').click(function() {
+	    	$(this).parent()
+	    	.insertAfter('iao-alert-start')
+	    	.fadeOut(500, function() {
+	    		$(this).remove();
+	    	});
+	    });
     	return this;
     };
 }( jQuery ));
