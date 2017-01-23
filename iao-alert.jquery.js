f329b9f274852a1b8864f5a38bd2e4196ecea159
Prashant Kapoor <prashant@hatwada.com>
2017-01-23 13:15:00 +0530

    Adding cross, fade out & removal time control js.

--- iao-alert.jquery.js
+++ iao-alert.jquery.js
@@ -1,7 +1,7 @@
 /*
  Plugin Name: iao alert
          Key: iao-alert
-     Version: 1.0.1
+     Version: 1.0.2
       Author: Prashant Kapoor
      Website: http://www.itsallonly.com
         Repo: https://github.com/Itsallonly/iao-alert
@@ -14,21 +14,27 @@
     	var opt = $.extend({
             msg: "This is default iao alert message.",
             type: "notification",
-            mode: "light"
+            mode: "light",
+            autoHide: true,
+            alertTime: "3000",
+            fadeTime: "500",
+            closeButton: true
         }, arr );
+        var closeOption = (opt.closeButton)?'<iao-alert-close></iao-alert-close>':'<style>iao-alert:before,iao-alert:after{display:none}</style>';
     	if($('iao-alert-box').length==0)
     	$('body').append('<iao-alert-box><iao-alert-start></iao-alert-start></iao-alert-box>');
-    	var iaoAlert = $('<iao-alert mode="'+opt.mode+'"type="'+opt.type+'">'+opt.msg+'<iao-alert-close></iao-alert-close></iao-alert>')
+    	var iaoAlert = $('<iao-alert mode="'+opt.mode+'"type="'+opt.type+'">'+opt.msg+closeOption+'</iao-alert>')
     	.insertAfter('iao-alert-start');
+    	if(opt.autoHide)
     	setTimeout(function(){
-    		iaoAlert.fadeOut(500, function() {
+    		iaoAlert.fadeOut(opt.fadeTime, function() {
 	    		$(this).remove();
 	    	});
-    	}, 3000);
+    	}, opt.alertTime);
     	$('iao-alert > iao-alert-close').click(function() {
 	    	$(this).parent()
 	    	.insertAfter('iao-alert-start')
-	    	.fadeOut(500, function() {
+	    	.fadeOut(opt.fadeTime, function() {
 	    		$(this).remove();
 	    	});
 	    });
