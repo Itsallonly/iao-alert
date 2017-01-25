025adc064ed89d7a1732ac091b426e5358a53ae8
wholensole <prashkapoor.18@gmail.com>
2017-01-25 13:01:19 +0530

    Added objective access to function.

--- iao-alert.jquery.js
+++ iao-alert.jquery.js
@@ -1,7 +1,7 @@
 /*
  Plugin Name: iao alert
          Key: iao-alert
-     Version: 1.0.4
+     Version: 1.0.5
       Author: Prashant Kapoor
      Website: http://www.itsallonly.com
         Repo: https://github.com/Itsallonly/iao-alert
@@ -10,8 +10,8 @@
   Dependency: Jquery
 */
 (function( $ ) {
-    $.fn.iaoAlert = function(arr) {
-      var opt = $.extend( {
+    $.fn.iaoAlert = $.iaoAlert = function(arr) {
+        var opt = $.extend( {
             msg: "This is default iao alert message.",
             type: "notification",
             mode: "light",
@@ -29,27 +29,27 @@
             closeOption : (opt.closeButton)?'<iao-alert-close></iao-alert-close>':'<style>#iao'+timeStamp+':before,#iao'+timeStamp+':after{display:none}</style>',
             chkMsg : (opt.msg.indexOf(" "))?'white-space:pre-wrap;word-wrap:break-word;':''
         };
-      if($('iao-alert-box').length==0)
-      $('body').append('<iao-alert-box position="top-left"><iao-alert-start></iao-alert-start></iao-alert-box><iao-alert-box position="top-right"><iao-alert-start></iao-alert-start></iao-alert-box><iao-alert-box position="bottom-right"><iao-alert-start></iao-alert-start></iao-alert-box><iao-alert-box position="bottom-left"><iao-alert-start></iao-alert-start></iao-alert-box>');
-      var iaoAlert = $('<iao-alert id="iao'+timeStamp+'" close-on-click='+opt.closeOnClick+' fade-on-hover='+opt.fadeOnHover+' mode="'+opt.mode+'"type="'+opt.type+'" style="'+ext.chkMsg+'">'+opt.msg+ext.closeOption+'</iao-alert>')
-      .insertAfter('iao-alert-box[position="'+ext.chkPosition+'"] > iao-alert-start');
-      if(opt.autoHide)
-      setTimeout(function(){
-        iaoAlert.fadeOut(opt.fadeTime, function() {
-          $(this).remove();
-        });
-      }, opt.alertTime);
+        if($('iao-alert-box').length==0)
+        $('body').append('<iao-alert-box position="top-left"><iao-alert-start></iao-alert-start></iao-alert-box><iao-alert-box position="top-right"><iao-alert-start></iao-alert-start></iao-alert-box><iao-alert-box position="bottom-right"><iao-alert-start></iao-alert-start></iao-alert-box><iao-alert-box position="bottom-left"><iao-alert-start></iao-alert-start></iao-alert-box>');
+        var iaoAlert = $('<iao-alert id="iao'+timeStamp+'" close-on-click='+opt.closeOnClick+' fade-on-hover='+opt.fadeOnHover+' mode="'+opt.mode+'"type="'+opt.type+'" style="'+ext.chkMsg+'">'+opt.msg+ext.closeOption+'</iao-alert>')
+        .insertAfter('iao-alert-box[position="'+ext.chkPosition+'"] > iao-alert-start');
+        if(opt.autoHide)
+        setTimeout(function(){
+            iaoAlert.fadeOut(opt.fadeTime, function() {
+                $(this).remove();
+            });
+        }, opt.alertTime);
         $('iao-alert[close-on-click="true"]').click(function() {
             $(this).fadeOut(opt.fadeTime, function() {
                 $(this).remove();
             });
         });
-      $('iao-alert > iao-alert-close').click(function() {
-        $(this).parent()
-        .fadeOut(opt.fadeTime, function() {
-          $(this).remove();
+        $('iao-alert > iao-alert-close').click(function() {
+            $(this).parent()
+            .fadeOut(opt.fadeTime, function() {
+                $(this).remove();
+            });
         });
-      });
-      return this;
+        return this;
     };
 }( jQuery ));
